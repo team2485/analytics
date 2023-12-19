114c0e08455823feffa752d4875a3179b4c1ef8d
Preston Seay <preston.seay@outlook.com>
2023-12-19 11:31:23 -0800

    picklist basics

--- src/app/picklist/page.js
+++ src/app/picklist/page.js
@@ -1,7 +1,53 @@
+'use client';
+
+import { useEffect, useState, useRef } from "react";
+
 export default function Picklist() {
+  const [fields, setFields] = useState([]);
+  const formRef = useRef();
+
+  useEffect(() => {
+    fetch('/api/get-numeric-fields')
+      .then(resp => resp.json())
+      .then(data => setFields(data));
+  }, []);
+  
+  function recalculate(event) {
+    event.preventDefault();
+    const formData = new FormData(formRef.current);
+    fetch('/api/compute-picklist', {
+      method: "POST",
+      body: JSON.stringify([...formData.entries()])
+    })
+      .then(resp => resp.json())
+      .then(data => console.log(data));
+  }
+
+  function Weights() {
+    if (fields.length > 0) {
+      return <>
+        {fields.map((field, index) => (
+          <div key={index}>
+            <label>{field} Weight: </label>
+            <input type="number" defaultValue={0} name={field}></input>
+          </div>
+        ))}
+      </>;
+
+    } else {
+      return <div>Loading Weight Fields...</div>
+    }
+
+  }
+
   return (
     <div>
       <h1>Picklist</h1>
+      <form ref={formRef} onSubmit={recalculate}>
+        <Weights></Weights>
+        {/* TODO: add team exclusion */}
+        <button>Reload Picklist</button>
+      </form>
     </div>
   )
 }
