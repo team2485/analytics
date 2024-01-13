1d8469d3e11452bbf18b0b941caa02276cd3292f
YaelBroudy <122691091+YaelBroudy@users.noreply.github.com>
2024-01-13 11:58:20 -0800

    Add Text Input Component

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,13 @@
+import TextInput from '@/components/TextInput';
+
 export default function Home() {
   return (
     <div>
       <h1>Scouting Form</h1>
+      <TextInput inputName={"Name"}/>
+      <TextInput />
+      <TextInput />
+      <TextInput />
     </div>
   )
 }
---
+++ src/components/TextInput.js
@@ -0,0 +1,8 @@
+export default function TextInput ({ inputName }) {
+    return (
+        <div>
+            <label>{inputName}</label>
+            <input type="text"></input>
+        </div>
+    )
+}
