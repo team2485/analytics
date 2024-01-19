66ac89cea719e5ddfd85bb1c35c7fd7e8b9b1f30
rmadiraju2026 <146482099+rmadiraju2026@users.noreply.github.com>
2024-01-18 20:28:00 -0800

    Fixed styles for TextInput

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -2,9 +2,10 @@ import styles from './TextInput.module.css'
 export default function TextInput ({ visibleName, internalName }) {
     return (
         <div>
-            <label htmlFor={internalName}>{visibleName}</label>
+            <label className={styles.font} htmlFor={internalName}>{visibleName}</label>
             <br></br>
             <input type="text" id={internalName} className={styles.textinput}></input>
+            <br></br>
         </div>
     )
 }
