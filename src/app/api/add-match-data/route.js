826806b9a356c58378f6f21c83652fc3f30a7da2
Riti Balam <ritibalam@Ritis-MacBook-Pro.local>
2024-01-30 20:03:37 -0800

    added all field checks in add-match-data/api

--- src/app/api/add-match-data/route.js
+++ src/app/api/add-match-data/route.js
@@ -11,17 +11,36 @@ export async function POST(req){
    }
 
    //if noshow add row
+   if(body.noshow){
+
+   }
 
    //check auto
+   if(!(_.isNumber(body.autoampscored)&&_.isNumber(body.autoampfailed)&&_.isNumber(body.autospeakerscored)&&_.isNumber(body.autospeakerfailed))){
+      return NextResponse.json({message: "Invalid Pre-Match Data!"}, {status: 400});
+   }
    //check tele
+   if(!(_.isNumber(body.teleampscored)&&_.isNumber(body.teleampfailed)&&_.isNumber(body.telenampedspeakerscored)&&_.isNumber(body.teleampedspeakerscored)&&_.isNumber(body.telespeakerfailed))){
+      return NextResponse.json({message: "Invalid Pre-Match Data!"}, {status: 400});
+   }
+
    //check end
+   if(!(_.isNumber(body.endlocation)&&_.isNumber(body.stageplacement)&&_.isBoolean(body.harmony)&&_.isNumber(body.trapScored)&&_.isNumber(body.trapFailed0))){
+      return NextResponse.json({message: "Invalid Pre-Match Data!"}, {status: 400});
+   }
    //check qualitative
+   if(!(_.isNumber(body.maneuverability)&&_.isNumber(body.aggression)&&_.isNumber(body.defenseevasion)&&_.isNumber(body.speakerspeed)&&_.isNumber(body.ampspeed)&&_.isBoolean(body.gndintake)&&_.isBoolean(body.srcintake)&&_.isNumber(body.isstagehazard)&&_.isNumber(body.trapspeed)&&_.isNumber(body.onstagespeed)&&_.isNumber(harmonyspeed))){
+      return NextResponse.json({message: "Invalid Pre-Match Data!"}, {status: 400});
+   }
    //check comments
+   if(!(_.isString(body.generalcomments)&&_.isString(body.breakdowncomments)&&_.isString(body.defensecomments))){
+      return NextResponse.json({message: "Invalid Pre-Match Data!"}, {status: 400});
+   }
 
    //add row
 
 
-   // let resp = await sql`INSERT INTO testmatches VALUES(${ScoutTeam}, ${teamStatus},${matchStatus})`;
+   let resp = await sql`INSERT INTO testmatches VALUES(${ScoutTeam}, ${Team},${Match}, ${Breakdown}, ${NoShow}, ${Leave}, ${AutoAmpScored}, ${AutoAmpFailed}, ${AutoSpeakerScored}, ${AutoSpeakerFailed},${TeleAmpScored}, ${TeleAmpFailed}, ${TeleNAmpedSpeakerScored}, ${TeleAmpedSpeakerScored}, ${TeleSpeakerFailed}, ${EndLocation}, ${StagePlacement}, ${Harmony}, ${TrapScored},${TrapFailed}, ${Maneuverablity}, ${Aggression}, ${DefenseEvasion}, ${SpeakerSpeed}, ${AmpSpeed}, ${GndIntake}, ${SrcIntake}, ${StageHazard}, ${TrapSpeed}, ${OnStageSpeed}, ${HarmonySpeed}, ${GeneralComments}, ${BreakdownComments}, ${DefenseComments})`;
    return NextResponse.json({message: "Success!"}, {status: 200});
 }
 
