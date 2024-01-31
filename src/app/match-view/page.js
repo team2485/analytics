baed80cde7389e10cca6c8f9c902947d428ebf4e
Preston Seay <preston.seay@outlook.com>
2024-01-30 19:48:40 -0800

    began match-view charts

--- src/app/match-view/page.js
+++ src/app/match-view/page.js
@@ -1,6 +1,8 @@
 "use client";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import styles from "./page.module.css";
+import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
 
 export default function MatchView() {
   //TODO: import match data from blue alliance
@@ -40,6 +42,52 @@ export default function MatchView() {
     return <div className={styles.matchTeam}>
       <h1>{teamData.team}</h1>
       <h2>{teamData.teamName}</h2>
+      <div className={styles.scoreBreakdownContainer}>
+        <div className={styles.espmBox}>{teamData.auto + teamData.tele + teamData.end}</div>
+        <div className={styles.espmBreakdown}>
+          <div>{teamData.auto}</div>
+          <div>{teamData.tele}</div>
+          <div>{teamData.end}</div>
+        </div>
+      </div>
+      <br></br>
+      <div className={styles.chartContainer}>
+        <h2>Average Note Placement</h2>
+        <ResponsiveContainer width="100%">
+        <BarChart
+          height={100}
+          data={[{
+            place: "Speaker",
+            value: teamData.avgNotes.speaker
+          },
+          {
+            place: "Amp'd Speaker",
+            value: teamData.avgNotes.ampedSpeaker
+          },
+          {
+            place: "Amp",
+            value: teamData.avgNotes.amp
+          },
+          {
+            place: "Trap",
+            value: teamData.avgNotes.trap
+          }]}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="place" />
+          <YAxis />
+          <Tooltip />
+          <Bar dataKey="value" fill="#66c" activeBar={<Rectangle fill="gold" stroke="purple" />} />
+        </BarChart>
+      </ResponsiveContainer>
+      </div>
+
     </div>
   }
 
