378d0b78ac09fe711de2f74c57452747bb963128
Preston Seay <preston.seay@outlook.com>
2024-02-02 20:20:59 -0800

    added queryparams & team buttons to alliance-view

--- src/app/match-view/page.js
+++ src/app/match-view/page.js
@@ -3,17 +3,27 @@ import { useEffect, useState } from "react";
 import styles from "./page.module.css";
 import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, PieChart, Pie, ResponsiveContainer, Cell } from 'recharts';
 import { VictoryPie } from "victory";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 
 
 export default function MatchView() {
   const [allData, setAllData] = useState(null);
   const [data, setData] = useState(null);
-  const [searchParams, setSearchParams] = useState(null);
+  const searchParams = useSearchParams();
+  const COLORS = [
+    ["#B7F7F2", "#A1E7E1", "#75C6BF", "#5EB5AE"],
+    ["#8AB8FD", "#7D99FF", "#6184DD", "#306BDD"],
+    ["#E1BFFA", "#E1A6FE", "#CA91F2", "#A546DF"],
+    ["#FFC6F6", "#ECA6E0", "#ED75D9", "#C342AE"],
+    ["#FABFC4", "#FEA6AD", "#F29199", "#E67983"],
+    ["#FFE3D3", "#EBB291", "#E19A70", "#D7814F"],
+  ];
 
   //get data
   useEffect(() => {
     //TODO: Get Data (from localstorage if cached recently)
-    //fetch("/api/get-alliance-data?team1=2485&team2=3485&team3=4485").then(resp => resp.json()).then(data => setData(data));
+    //fetch("/api/get-alliance-data").then(resp => resp.json()).then(data => setData(data));
     setAllData({
       "2485": {
         team: 2485,
@@ -34,20 +44,36 @@ export default function MatchView() {
           onstageHarmony: 60,
         },
       },
+      "9485": {
+        team: 9485,
+        teamName: "W.A.R. Lords 2",
+        auto: 10,
+        tele: 17,
+        end: 10,
+        avgNotes: {
+          speaker: 1,
+          ampedSpeaker: 5,
+          amp: 0,
+          trap: 1,
+        },
+        endgame: {
+          none: 5,
+          park: 45,
+          onstage: 30,
+          onstageHarmony: 20,
+        },
+      },
     })
   }, []);
 
-  //get search params
+    //setData based on teams selected
   useEffect(() => {
-    setSearchParams(new URLSearchParams(typeof window == "undefined" ? "" : window.location?.search || ""));
-  }, []);
-
-  useEffect(() => {
-    if (searchParams) {
-      let [team1, team2, team3, team4, team5, team6] = [searchParams.get("team1"), searchParams.get("team2"), searchParams.get("team3"), searchParams.get("team4"), searchParams.get("team4"), searchParams.get("team6")];
+    if (searchParams && allData) {
+      let [team1, team2, team3, team4, team5, team6] = [searchParams.get("team1"), searchParams.get("team2"), searchParams.get("team3"), searchParams.get("team4"), searchParams.get("team5"), searchParams.get("team6")];
+      console.log(searchParams.get("team1"));
       setData({team1: allData[team1], team2: allData[team2], team3: allData[team3], team4: allData[team4], team5: allData[team5], team6: allData[team6]});
     }
-  }, [searchParams])
+  }, [searchParams, allData]);
 
   //until url loads show loading
   if (!data || searchParams == null) {
@@ -59,28 +85,42 @@ export default function MatchView() {
   //show form if systems are not a go
   if (searchParams.get("go") != "go") {
     return <div>
-      <form>
-        <label htmlFor="team1">Team 1:</label>
+      <form className={styles.teamForm}>
+        <label htmlFor="team1">Blue 1:</label>
         <input id="team1" name="team1" defaultValue={searchParams.get("team1")}></input>
         <br></br>
-        <label htmlFor="team2">Team 2:</label>
+        <label htmlFor="team2">Blue 2:</label>
         <input id="team2" name="team2" defaultValue={searchParams.get("team2")}></input>
         <br></br>
-        <label htmlFor="team3">Team 3:</label>
+        <label htmlFor="team3">Blue 3:</label>
         <input id="team3" name="team3" defaultValue={searchParams.get("team3")}></input>
         <br></br>
-        <label htmlFor="team4">Team 4:</label>
+        <label htmlFor="team4">Red 1:</label>
         <input id="team4" name="team4" defaultValue={searchParams.get("team4")}></input>
         <br></br>
-        <label htmlFor="team5">Team 5:</label>
+        <label htmlFor="team5">Red 2:</label>
         <input id="team5" name="team5" defaultValue={searchParams.get("team5")}></input>
         <br></br>
-        <label htmlFor="team6">Team 6:</label>
+        <label htmlFor="team6">Red 3:</label>
         <input id="team6" name="team6" defaultValue={searchParams.get("team6")}></input>
         <br></br>
         <input type="hidden" name="go" value="go"></input>
         <button>Go!</button>
       </form>
+    </div>
+  }
+
+  function AllianceBoard({t1, t2, t3, colors}) {
+    return <div className={styles.allianceBoard}>
+      <Link href={"/team-view?team=" + t1.team}>
+        <button style={{background: colors[0][1]}}>{t1.team}</button>
+      </Link>
+      <Link href={"/team-view?team=" + t2.team}>
+      <button style={{background: colors[1][1]}}>{t2.team}</button>
+      </Link>
+      <Link href={"/team-view?team=" + t3.team}>
+      <button style={{background: colors[2][1]}}>{t3.team}</button>
+      </Link>
     </div>
   }
 
@@ -156,15 +196,19 @@ export default function MatchView() {
   //if data is in, show interface
   return (
     <div>
-      <div className={styles.matches}>
-        <TeamDisplay teamData={data.team1} colors={["#B7F7F2", "#A1E7E1", "#75C6BF", "#5EB5AE"]}></TeamDisplay>
-        <TeamDisplay teamData={data.team1} colors={["#8AB8FD", "#7D99FF", "#6184DD", "#306BDD"]}></TeamDisplay>
-        <TeamDisplay teamData={data.team1} colors={["#E1BFFA", "#E1A6FE", "#CA91F2", "#A546DF"]}></TeamDisplay>
+      <div className={styles.matchOverview}>
+        <AllianceBoard t1={data.team1} t2={data.team2} t3={data.team3} colors={[COLORS[0], COLORS[1], COLORS[2]]}></AllianceBoard>
+        <AllianceBoard t1={data.team4} t2={data.team5} t3={data.team6} colors={[COLORS[3], COLORS[4], COLORS[5]]}></AllianceBoard>
       </div>
       <div className={styles.matches}>
-        <TeamDisplay teamData={data.team1} colors={["#FFC6F6", "#ECA6E0", "#ED75D9", "#C342AE"]}></TeamDisplay>
-        <TeamDisplay teamData={data.team1} colors={["#FABFC4", "#FEA6AD", "#F29199", "#E67983"]}></TeamDisplay>
-        <TeamDisplay teamData={data.team1} colors={["#FFE3D3", "#EBB291", "#E19A70", "#D7814F"]}></TeamDisplay>
+        <TeamDisplay teamData={data.team1} colors={COLORS[0]}></TeamDisplay>
+        <TeamDisplay teamData={data.team2} colors={COLORS[1]}></TeamDisplay>
+        <TeamDisplay teamData={data.team3} colors={COLORS[2]}></TeamDisplay>
+      </div>
+      <div className={styles.matches}>
+        <TeamDisplay teamData={data.team4} colors={COLORS[3]}></TeamDisplay>
+        <TeamDisplay teamData={data.team5} colors={COLORS[4]}></TeamDisplay>
+        <TeamDisplay teamData={data.team6} colors={COLORS[5]}></TeamDisplay>
       </div>
     </div>
   )
--- src/app/team-view/page.js
+++ src/app/team-view/page.js
@@ -1,7 +1,13 @@
+"use client";
+
+import { useSearchParams } from "next/navigation"
+
 export default function TeamView() {
+  const searchParams = useSearchParams();
+  let team = searchParams.get("team");
   return (
     <div>
-      <h1>Team View</h1>
+      <h1>Team View for {team}</h1>
     </div>
   )
 }
