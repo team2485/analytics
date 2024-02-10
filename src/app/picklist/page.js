a6d476f62834f8128296b76e172acc7b44367373
YaelBroudy <122691091+YaelBroudy@users.noreply.github.com>
2024-02-10 12:01:14 -0800

    add minimal weight styles

--- src/app/picklist/page.js
+++ src/app/picklist/page.js
@@ -1,5 +1,6 @@
 'use client';
 
+import styles from "./page.module.css";
 import { useEffect, useState, useRef } from "react";
 
 export default function Picklist() {
@@ -26,14 +27,17 @@ export default function Picklist() {
 
   function Weights() {
     if (fields.length > 0) {
-      return <>
+      return <div className={styles.allWeights}>
         {fields.map((field, index) => (
-          <div key={index}>
+          <div key={index} className={styles.weight}>
+            <table>
+              
+            </table>
             <label>{field} Weight: </label>
             <input type="number" defaultValue={0} name={field}></input>
           </div>
         ))}
-      </>;
+      </div>;
 
     } else {
       return <div>Loading Weight Fields...</div>
@@ -42,9 +46,10 @@ export default function Picklist() {
   }
 
   return (
-    <div>
+    <div className={styles.MainDiv}>
       <h1>Picklist</h1>
       <form ref={formRef} onSubmit={recalculate}>
+        <span>Weights</span>
         <Weights></Weights>
         {/* TODO: add team exclusion */}
         <button>Reload Picklist</button>
