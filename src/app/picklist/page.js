9389b7bfb58870e3bb41d0373cda2ce575fed58a
YaelBroudy <122691091+YaelBroudy@users.noreply.github.com>
2024-02-10 14:35:35 -0800

    formatted weights and added alliance table

--- src/app/picklist/page.js
+++ src/app/picklist/page.js
@@ -27,17 +27,26 @@ export default function Picklist() {
 
   function Weights() {
     if (fields.length > 0) {
-      return <div className={styles.allWeights}>
-        {fields.map((field, index) => (
-          <div key={index} className={styles.weight}>
-            <table>
-              
-            </table>
-            <label>{field} Weight: </label>
-            <input type="number" defaultValue={0} name={field}></input>
-          </div>
-        ))}
-      </div>;
+      return <table className={styles.weightsTable}>
+          <tr>
+              <td><label htmlFor="ESPM">ESPM:</label></td>
+              <td><input type="number" defaultValue={0} name="ESPM"></input></td>
+              <td><label htmlFor="end">End:</label> </td>
+              <td><input type="number" defaultValue={0} name="end"></input></td>
+          </tr>
+          <tr>
+              <td><label htmlFor="auto">Auto:</label></td>
+              <td><input type="number" defaultValue={0} name="auto"></input></td>
+              <td><label htmlFor="speed">Speed:</label> </td>
+              <td><input type="number" defaultValue={0} name="speed"></input></td>
+          </tr>
+          <tr>
+              <td><label htmlFor="tele">Tele:</label></td>
+              <td><input type="number" defaultValue={0} name="tele"></input></td>
+              <td><label htmlFor="movement">Movement:</label> </td>
+              <td><input type="number" defaultValue={0} name="movement"></input></td>
+          </tr>
+      </table>;
 
     } else {
       return <div>Loading Weight Fields...</div>
@@ -45,23 +54,49 @@ export default function Picklist() {
 
   }
 
+  function AllianceRow({alliance}) {
+    return (
+      <tr>
+          <td>A{alliance}</td>
+          <td><label htmlFor={"T1A" + alliance}></label><input name={"T1A" + alliance} type="number"></input></td>
+          <td><label htmlFor={"T2A" + alliance}></label><input name={"T2A" + alliance} type="number"></input></td>
+          <td><label htmlFor={"T3A" + alliance}></label><input name={"T3A" + alliance} type="number"></input></td>
+      </tr>
+    )
+  };
+
   return (
     <div className={styles.MainDiv}>
-      <h1>Picklist</h1>
       <form ref={formRef} onSubmit={recalculate}>
-        <span>Weights</span>
-        <Weights></Weights>
+        <div className={styles.weights}>
+          <span><b>Weights</b></span>
+          <Weights></Weights>
+        </div>
         {/* TODO: add team exclusion */}
         <button>Reload Picklist</button>
       </form>
+      <div className={styles.alliances}>
+        <span><b>Alliances</b></span>
+        <table className={styles.allianceTable}>
+          <th></th><th>T1</th><th>T2</th><th>T3</th>
+          <AllianceRow alliance={"1"}></AllianceRow>
+          <AllianceRow alliance={"2"}></AllianceRow>
+          <AllianceRow alliance={"3"}></AllianceRow>
+          <AllianceRow alliance={"4"}></AllianceRow>
+          <AllianceRow alliance={"5"}></AllianceRow>
+          <AllianceRow alliance={"6"}></AllianceRow>
+          <AllianceRow alliance={"7"}></AllianceRow>
+          <AllianceRow alliance={"8"}></AllianceRow>
+        </table>
+      </div>
       <table>
-        <tr><th>Team</th><th>Scout Name</th><th>Score</th><th>ESPM</th></tr>
+        <tr><th>Team</th><th>Score</th><th>ESPM</th><th>Mvt</th></tr>
         {picklist.map((teamData, index) => (
           <tr key={index}>
             <td>{teamData}</td>
-            <td>{teamData.scoutname}</td>
             <td>{teamData.score}</td>
             <td>{teamData.espm}</td>
+            <td>{teamData.movement}</td>
           </tr>
         ))}
       </table>
