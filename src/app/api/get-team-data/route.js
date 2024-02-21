4da6c87285d1a26db763c642e11587d2735b7a56
Preston Seay <preston.seay@outlook.com>
2024-02-20 22:27:57 -0800

    added data defaults

--- src/app/api/get-team-data/route.js
+++ src/app/api/get-team-data/route.js
@@ -124,7 +124,61 @@ export async function get(request) {
   }
   let returnObject = {team: team, teamName: '', autoScore: 0, teleScore: 0, endScore: 0, espmOverTime: [],
                         noShow: 0, breakdown: 0, lastBreakdown: 0, matchesScouted: rows.length(), scouts: rowsToArray(rows, "scoutName"),
-                        generalComments: rowsToArray(rows, "generalComments"), breakdownComments: rowsToArray(rows, "breakdownComments"), defenseComments: rowsToArray(rows, "defenseComments")};
+                        generalComments: rowsToArray(rows, "generalComments"), breakdownComments: rowsToArray(rows, "breakdownComments"), defenseComments: rowsToArray(rows, "defenseComments"),
+                        auto: {
+                            leave: 0,
+                            autoOverTime: [],
+                            autoNotes: {
+                                total: 0,
+                                ampSuccess: 0,
+                                ampAvg: 0,
+                                spkrSuccess: 0,
+                                spkrAvg: 0
+                            }},
+                        tele: {
+                            teleOverTime: [],
+                            teleNotes: {
+                                amplified: 0,
+                                total: 0,
+                                ampSuccess: 0,
+                                ampAvg: 0,
+                                spkrSuccess: 0,
+                                spkrAvg: 0
+                            },
+                        },
+                        endgame: {
+                            stage: {
+                                none: 0,
+                                park: 0,
+                                onstage: 0,
+                                onstageHarmony: 0,
+                            },
+                            onstageAttempt: 0,
+                            onstageSuccess: 0,
+                            harmonySuccess: 0,
+                            onstagePlacement: {
+                                center: 0,
+                                side: 0,
+                            },
+                            trapSuccess: 0,
+                            trapAvg: 0,
+                        },
+                        intake: {
+                            ground: false,
+                            source: false,
+                        },
+                        qualitative: [
+                            {name: "Onstage Speed", rating: 0},
+                            {name: "Harmony Speed", rating: 0},
+                            {name: "Trap Speed", rating: 0},
+                            {name: "Amp Speed", rating: 0},
+                            {name: "Apeaker Speed", rating: 0},
+                            {name: "Stage Hazard", rating: 0},
+                            {name: "Defense Evasion", rating: 0},
+                            {name: "Aggression", rating: 0},
+                            {name: "Maneuverability", rating: 0},
+                        ],
+                    };
   if (returnObject.matchesScouted == 0) {
     return NextResponse.json(returnObject, {status: 200});
   }
