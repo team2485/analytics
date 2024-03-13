989f1df3e67e7bf966f759a162bcaed703f95c58
Preston Seay <preston.seay@outlook.com>
2024-03-12 22:19:30 -0700

    added password to env

--- src/app/api/delete-row/route.js
+++ src/app/api/delete-row/route.js
@@ -6,7 +6,7 @@ export async function POST(request) {
   const res = await request.json();
   const { id, password } = res;
 
-  if (password !== "delete") {
+  if (password !== process.env.SUDO_PASSWORD) {
     return NextResponse.json({error: "Invalid password"}, {status: 401});
   }
   if (!_.isInteger(id)) {
