59d5d033fda4dfbe65d6a02a250dfcc6e490c722
YaelBroudy <122691091+YaelBroudy@users.noreply.github.com>
2024-07-31 19:07:36 -0700

    champs2024 to batb2024

--- src/app/api/add-match-data/route.js
+++ src/app/api/add-match-data/route.js
@@ -13,7 +13,7 @@ export async function POST(req){
    //if noshow add row :)  :(
    if(body.noshow){
       console.log('no show!');
-      let resp = await sql`INSERT INTO champs2024 (ScoutName, ScoutTeam, Team, Match, NoShow) VALUES(${body.scoutname}, ${body.scoutteam}, ${body.team}, ${body.match}, ${body.noshow})`;
+      let resp = await sql`INSERT INTO batb2024 (ScoutName, ScoutTeam, Team, Match, NoShow) VALUES(${body.scoutname}, ${body.scoutteam}, ${body.team}, ${body.match}, ${body.noshow})`;
       return NextResponse.json({message: "Success!"}, {status: 201});
    }
 
@@ -42,7 +42,7 @@ console.log(body.defensecomments)
    //add row
   
   
-   let resp = await sql`INSERT INTO champs2024 (ScoutName, ScoutTeam, Team, Match, Breakdown, NoShow, Leave, AutoAmpScored, AutoAmpFailed, AutoSpeakerScored, AutoSpeakerFailed, PassedNotes, TeleAmpScored, TeleAmpFailed, TeleNAmpedSpeakerScored, TeleAmpedSpeakerScored, TeleSpeakerFailed, EndLocation, StagePlacement, Harmony, TrapScored, TrapFailed, Maneuverability, Aggression, DefenseRating, DefenseEvasion, SpeakerSpeed, AmpSpeed, GndIntake, SrcIntake, StageHazard, TrapSpeed, OnStageSpeed, HarmonySpeed, GeneralComments, BreakdownComments, DefenseComments)
+   let resp = await sql`INSERT INTO batb2024 (ScoutName, ScoutTeam, Team, Match, Breakdown, NoShow, Leave, AutoAmpScored, AutoAmpFailed, AutoSpeakerScored, AutoSpeakerFailed, PassedNotes, TeleAmpScored, TeleAmpFailed, TeleNAmpedSpeakerScored, TeleAmpedSpeakerScored, TeleSpeakerFailed, EndLocation, StagePlacement, Harmony, TrapScored, TrapFailed, Maneuverability, Aggression, DefenseRating, DefenseEvasion, SpeakerSpeed, AmpSpeed, GndIntake, SrcIntake, StageHazard, TrapSpeed, OnStageSpeed, HarmonySpeed, GeneralComments, BreakdownComments, DefenseComments)
    VALUES (${body.scoutname}, ${body.scoutteam}, ${body.team}, ${body.match}, ${body.breakdown}, ${body.noshow}, ${body.leave}, ${body.autoampscored}, ${body.autoampfailed}, ${body.autospeakerscored}, ${body.autospeakerfailed}, ${body.passednotes}, ${body.teleampscored}, ${body.teleampfailed}, ${body.telenampedspeakerscored}, ${body.teleampedspeakerscored}, ${body.telespeakerfailed}, ${body.endlocation}, ${body.stageplacement}, ${body.harmony}, ${body.trapscored}, ${body.trapfailed}, ${body.maneuverability}, ${body.aggression}, ${body.defenserating}, ${body.defenseevasion}, ${body.speakerspeed}, ${body.ampspeed}, ${body.gndintake}, ${body.srcintake}, ${body.stagehazard}, ${body.trapspeed}, ${body.onstagespeed}, ${body.harmonyspeed}, ${body.generalcomments}, ${body.breakdowncomments}, ${body.defensecomments})`;
    return NextResponse.json({message: "Success!"}, {status: 201});
 }
--- src/app/api/get-team-data/route.js
+++ src/app/api/get-team-data/route.js
@@ -15,7 +15,7 @@ export async function GET(request) {
     return NextResponse.json({message: "ERROR: Invalid team number"}, {status: 400});
   }
 
-  let data = await sql`SELECT * FROM champs2024 WHERE team = ${team};`;
+  let data = await sql`SELECT * FROM batb2024 WHERE team = ${team};`;
   const rows = data.rows;
 
   if (rows.length == 0) {
